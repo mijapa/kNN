2ae314471e8830786fab2395ac5f05ffcd69f6bc
Michał <michaljanpatyk@gmail.com>
2018-10-29 12:21:07 +0100

    test policzDystIsortuj

--- Main.py
+++ Main.py
@@ -19,6 +19,10 @@ def distManhattan(a, b):
     dist = sp.spatial.distance.yule(a, b)
     return dist
 
+
+# TODO BONUS: implementacja metryki Manhattan
+# TODO BONUS: implementacja korelacji Persona
+
 class kNN:
     def __init__(self, parametr_k, dane_etykiety_learning):
         self.dane_etykiety_learning = dane_etykiety_learning
@@ -26,10 +30,10 @@ class kNN:
         self.etykiety_learning = dane_etykiety_learning[:, -1:]
         self.parametr_k = parametr_k
 
-    def policzDystIsortuj(self, a, fcjaDist):
+    def policzDystIsortuj(self, dana_test, fcjaDist):
         y = []
         for x in self.dane_learning:
-            y += [[fcjaDist(a, x)]]
+            y += [[fcjaDist(dana_test, x)]]
         # print(y)
         y = np.append(self.dane_etykiety_learning, y, axis=1)
         z = y[y[:, -1].argsort()]
@@ -62,13 +66,16 @@ class kNN:
             a += 1
         return b / (a)  # jakiej wielkosci powinno być a?
 
-dane_etykiety_learning = importer('iris.data.learning')
-dane_etykiety_test = importer('iris.data.test')
+        # dane_etykiety_learning = importer('iris.data.learning')
+        # dane_etykiety_test = importer('iris.data.test')
 
-dane_test = dane_etykiety_test[:, :-1]
-etykiety_test = dane_etykiety_test[:, -1:]
+        # dane_test = dane_etykiety_test[:, :-1]
+        # etykiety_test = dane_etykiety_test[:, -1:]
 
-nowa = kNN(1, dane_etykiety_learning)
+        # nowa = kNN(1, dane_etykiety_learning)
 # print(nowa.predict(dane_test, distEucli))
-print("Procent poprawnych rozpoznań: ")
-print(nowa.score(dane_test, etykiety_test, distEucli))
+
+    # print("Procent poprawnych rozpoznań: ")
+    # print(nowa.score(dane_test, etykiety_test, distEucli))
+
+    #print(dane_etykiety_learning[:2])
--- test_KNN.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-class TestKNN(TestCase):
-    def test_predict(self):
-        self.fail()
-
-    def test_score(self):
-        self.fail()
